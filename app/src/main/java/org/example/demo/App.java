4d73fb4177eacc2ceea1f61897e537a16a007771
yunjeoming <yunjeoming@gmail.com>
2022-04-03 19:30:38 +0900

    Init prj

---
+++ app/src/main/java/org/example/demo/App.java
@@ -0,0 +1,15 @@
+package org.example.demo;
+
+import org.example.demo.common.SystemLogger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SystemLogger.println( "Hello World!" );
+    }
+}
---
+++ common/src/main/java/org/example/demo/common/SystemLogger.java
@@ -0,0 +1,9 @@
+package org.example.demo.common;
+
+public class SystemLogger 
+{
+    public static void println(String message)
+    {
+        System.out.println(message);
+    }
+}
